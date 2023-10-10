75a664c671ccd3c456ae2a3de79864ead209c925
sumaximize <hellosumax@gmail.com>
2023-10-11 03:40:24 +0530

    added project readme, code files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# instacart-order-management-data-model
-Designing a data model for Instacart's (American retail company) order management system
+# Instacart's Order Management Data Model
+
+![instacart logo](./instacart-logo.png)
+
+Objective: to design a data model for Instacart's (American retail company) order management system.
+
+**Data Model Design**
+![order management data model design](./instacart_order_management_data_model.png)
+
+**Tables:**
+
+1. _aisles_: details on where product is shelved in store eg. "breakfast bakery", "fresh vegetables", "refrigerated" etc
+2. _departments_: product grouping eg. "frozen", "bakery", "produce", "pets" etc.
+3. _order_products_: data which specifies if product has been reordered i.e if it was ordered in the past
+4. _orders_: order details
+5. _products_: product details
+
+For detailed information on the data tables, please refer to [dataset source on Kaggle](https://www.kaggle.com/competitions/instacart-market-basket-analysis/data).
+
+**Files**
+- _instacart_data_model.sql_: code to create tables along with column constraints
+- _instacart_order_management_data_model.ipynb_: code to create tables with constraints and to bulk upload the flat file data to the database tables
+
+**NB**: we have used the opensource _Postgresql_ as our choice of database.
---
+++ instacart_data_model.sql
@@ -0,0 +1,65 @@
+
+CREATE TABLE IF NOT EXISTS aisles
+(
+    aisle_id serial PRIMARY KEY,
+    aisle character varying COLLATE pg_catalog."default"
+);
+
+CREATE TABLE IF NOT EXISTS departments
+(
+    department_id serial PRIMARY KEY,
+    department character varying COLLATE pg_catalog."default"
+);
+
+CREATE TABLE IF NOT EXISTS order_products
+(
+    order_id integer NOT NULL,
+    product_id integer NOT NULL,
+    add_to_cart_order integer,
+    reordered integer,
+    CONSTRAINT order_products_pkey PRIMARY KEY (order_id, product_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    order_id serial PRIMARY KEY,
+    user_id integer,
+    order_number integer,
+    order_dow integer,
+    order_hour_of_day integer,
+    days_since_prior_order integer
+);
+
+CREATE TABLE IF NOT EXISTS products
+(
+    product_id serial PRIMARY KEY,
+    product_name character varying COLLATE pg_catalog."default",
+    aisle_id integer,
+    department_id integer
+);
+
+ALTER TABLE IF EXISTS order_products
+    ADD CONSTRAINT order_products_order_id_fkey FOREIGN KEY (order_id)
+    REFERENCES orders (order_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+ALTER TABLE IF EXISTS order_products
+    ADD CONSTRAINT order_products_product_id_fkey FOREIGN KEY (product_id)
+    REFERENCES products (product_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+ALTER TABLE IF EXISTS products
+    ADD CONSTRAINT products_aisle_id_fkey FOREIGN KEY (aisle_id)
+    REFERENCES aisles (aisle_id) MATCH SIMPLE
+	ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+ALTER TABLE IF EXISTS products
+    ADD CONSTRAINT products_department_id_fkey FOREIGN KEY (department_id)
+    REFERENCES departments (department_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+END;
